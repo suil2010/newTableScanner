cdb70dc96d2b130b9d20a33aa14ca18be07f85ab
hp <hp@192.168.0.104>
2017-12-02 15:51:02 +0900

    aaa

--- noshow/src/main/webapp/WEB-INF/etc/test.sql
+++ noshow/src/main/webapp/WEB-INF/etc/test.sql
@@ -4,3 +4,9 @@ UPDATE AUTHORITY set AUTHORITY = 'ROLE_MEMBER' WHERE member_id = 'rlaghrb';
 select * from AUTHORITY;
 select * from member;
 UPDATE member set drop_check = 1 WHERE member_id = 'dlwltn1';
+
+
+
+
+
+

d302f750252df3422e4226e061be86b349a07fae
YoonDongUng <qwe7662@naver.com>
2017-12-09 19:46:40 +0900

    test.sql//

--- noshow/src/main/webapp/WEB-INF/etc/test.sql
+++ noshow/src/main/webapp/WEB-INF/etc/test.sql
@@ -16,3 +16,12 @@ UPDATE member set member_name = "dkssudgktpdy" WHERE member_id = '201234026';
 
 select * from reservation;
 select * from restaurant
+
+insert into MEMBER(member_id, member_password, member_name, member_gender, member_tel, member_birthday, member_email) values('phj','phj','박현준','남','02132',sysdate,'djd' );
+insert into RESTAURANT values('phj',1,8,18,2012121,'현준이네고기집','010212132',sysdate,sysdate,'ahffk','ahffk',30,5000);
+
+select a.business_id, b.code_val, c.code_val, d.code_val
+from RESTAURANT a, rt_code b, rt_code c, rt_code d
+where a.rt_holiday = b.code_num and 
+	  a.rt_field = c.code_num and
+	  a.rt_term = d.code_num;
